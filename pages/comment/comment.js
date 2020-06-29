97791a61c8cedd25b2b9a88f2d328a1cacc3d520
myownbourne <810060354@qq.com>
2020-06-29 16:27:44 +0800

    comment2

--- pages/comment/comment.js
+++ pages/comment/comment.js
@@ -5,14 +5,65 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    gid:null,
+    oid:null,
+    comment:null,
+    input:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.setData({
+      gid:options.gid,
+      oid:options.oid
+    })
+    this.get_comment()
+  },
+  get_comment(){
+    wx.request({
+      url: 'http://brucemarkdown.top:5000/get_comment',
+      data:{
+        gid:this.data.gid,
+        oid:this.data.oid
+      },
+      method:'POST',
+      success:(res)=>{
+        this.setData({
+          comment:res.data
+        })
+      }
+    })
+  },
 
+  setcomment(){
+    let input = this.data.input
+    wx.request({
+      url: 'http://brucemarkdown.top:5000/set_comment',
+      data:{
+        input:this.data.input,
+        oid:this.data.oid,
+        gid:this.data.gid
+      },
+      method:'POST',
+      success:(res)=>{
+        if(res.data=='succeed'){
+          wx: wx.showToast({
+            title: '设置成功'
+          })
+          this.setData({
+            comment:input,
+            input:''
+          })
+        }
+      }
+  })
+},
+  getinput(e){
+    this.setData({
+      input:e.detail.value
+    })
   },
 
   /**
